05f2b02f38ad5609c655fcf044d789a558cc8778
Amy Jiang <amyjianggg@gmail.com>
2017-05-28 22:46:53 -0700

    fixed: squares & randomSquareGenerator
    fixed so now we can generate at a random speed; still need to create more than one square in each column.

--- RythmnProject.ino
+++ RythmnProject.ino
@@ -4,6 +4,11 @@
 #include "TVOlogo.h"
 
 TVout TV;
+TVout Screen;
+
+double randomNumber;
+
+
 // O indicates off
 int buttonSwitchOne = 0; // buttonOne
 int buttonSwitchTwo = 0; // buttonTwo
@@ -14,141 +19,146 @@ int testScore = 0; // holds the score
 double x = 8.25; //first column's X
 double x2 = 38.125; // second column's X
 double x3 = 67.875; //third column's X
-
 double y = 10;
 double y2 = 10;
 double y3 = 10;
+double velY[3] = {y,y2,y3};
 
-const int buttonOne = 2; 
+const int buttonOne = 6; 
 const int buttonTwo = 4;
-const int buttonThree = 6;
+const int buttonThree = 2;
 
-double speedController = 0; // controls the speed
+double speedController = 0.0; // controls the speed
 
 //start of setup function
+boolean button1Status = false;
+
 void setup() {
   TV.begin(NTSC,120,96); //TV, X coor , Y coor
   pinMode(buttonOne,INPUT);
   pinMode(buttonTwo,INPUT);
   pinMode(buttonThree,INPUT);
 
-  drawMenu();
-
-
-   // TV.clear_screen();
-
-  // createGUI(); // create table
+  
+  delay(1000);
   
 }// end of setup function
 
 
 // start of loop function
 void loop() {
+  //drawMenu();
   createGUI(); // create table
-    
+  TV.print(90,17, testScore); // display running score ---
+
   buttonSwitchOne = digitalRead(buttonOne);
   buttonSwitchTwo = digitalRead(buttonTwo);
   buttonSwitchThree = digitalRead(buttonThree);
 
-  readButtonOne(x, y, buttonSwitchOne);
-  readButtonTwo(x2, y2, buttonSwitchTwo);
-  readButtonThree(x3,y3, buttonSwitchThree);
+  readButton(x, y,buttonSwitchOne);
+  readButton(x2,y2,buttonSwitchTwo);
+  readButton(x3,y3,buttonSwitchThree);
 
-  TV.select_font(font4x6);
-
- TV.print(90,17, testScore); // display running score ---
  speedController = testScore/10;
 
- if(speedController <= 0 || speedController > 10)
- {
-  speedController = 1;
+ if(speedController <= 0.0 || speedController > 5.0) {
+  speedController = 1.0;
  }
 
- y2 = y2 + speedController;
- createSquare(x,y++,10,10,1); // create moving square 1
- createSquare(x2,y2,10,10,1); // create moving square 2
- createSquare(x3,y3++,10,10,1); // create moving square 2
+ //y2 = y2 + speedController;
+  y = y + velY[0];
+  y2 = y2 + velY[1];
+  y3 = y3 + velY[2];
 
- if(y > 100) // sets y value back at 10
- {
-   y = 10; // position of box
- }
- if(y2 > 100) // sets y2 value back at 10
- {
-     y2= 10 ;  
- }
- if(y3 > 100) // sets y3 value back at 10
- {
-    y3= 10 ;  
- }
- 
+  createSquare(x,y,10,10,1); // create moving square 1
+  createSquare(x2,y2,10,10,1); // create moving square 2
+  createSquare(x3,y3,10,10,1); // create moving square 2
+
+  
+
+  speedGenerator(y,0);
+  speedGenerator(y2,1);
+  speedGenerator(y3,2);
+
+
   TV.delay(16);
 
-//  TV.clear_screen(); // clear the moving square
 
-} 
-//end of loop function
+// TV.clear_screen(); // clear the moving square
+
+} //end of loop function
 
 
-void readButtonOne(double x, double y,int buttonOne) 
-{ // open curly brace function
- if(buttonOne == HIGH) 
-  { // first curly brace
+void speedGenerator(double &y, int index) { //resets Y value to top if it passes y = 100
+  if(y >= 100.0) { 
+    y = 10.0;//when y value reaches end (100) reset y to top
+
+    //generate randomNumber
+    randomNumber = random(1.0,2.5);
+    randomNumber += random(0.0,99.0)/100.0;
+    TV.print(90,90, randomNumber); // display randomNumber
+    velY[index] = randomNumber; 
+  }
+}
+
+void readButton(double generalX, double y, int generalButton) 
+{
+   if (y < 78){
+      if(generalButton == HIGH) {
+        TV.print(generalX,0,"MISS");
+        testScore--;
+     }
+    }
+  else if(generalButton == HIGH && generalX == 8.25) { 
+      if(y <= 89 && y >= 78){
+        TV.print(8.25,0,"GREAT!");
+        testScore++;
+  } 
+    else if(generalButton == HIGH && generalX == 38.125){
+      if(y <= 89 && y >= 78 ) {
+        TV.print(38.125,0,"GREAT!");
+        testScore++;
+      }
+    }
+    else if(generalButton == HIGH && generalX == 69.875){
+      if(y <= 89 && y >= 78 ) {
+        TV.print(69.875,0,"GREAT!");
+        testScore++;
+      }
+    }
+  }
+}
+
+int generateRandomSquares(){
+    double x = 0.0;
+    int randNumber = random(1,4);
+    int columnSquareGenerator = random(1,4);
+
+    if(randNumber == 1) {
+      //create one square
+      switch(columnSquareGenerator){
+       case 1: x = 8.25; createSquare(x,y++,10,10,1); break;
+       case 2: x = 38.125; createSquare(x,y++,10,10,1); break;
+       case 3: x = 67.875; createSquare(x,y++,10,10,1); break;
+      }
+    }
+    else if (randNumber == 2) {
+      //create two squares
+       switch(columnSquareGenerator){
+       case 1: x = 8.25; createSquare(x,y++,10,10,1); break;
+       case 2: x = 38.125; createSquare(x,y++,10,10,1); break;
+       case 3: x = 67.875; createSquare(x,y++,10,10,1); break;
+      }
+    }
+    else if(randNumber == 3) {
+      //create three squares
+    }
     
-      if(y <= 89 && y >= 78 && x == 8.25) // first square
-      { // curly if y
-          TV.print(5.25,0,"GREAT!");
-          testScore++;
-        } 
-        else {
-          TV.print(5.25,0,"MISS");
-          testScore--;
-        }
-        
-  } // end first curly brace
-} // ending curly brace function
-
-void readButtonTwo(double x2, double y2,int buttonTwo) 
-{ // open curly brace function
- if(buttonTwo == HIGH) 
-  { // first curly brace
-    
-        if ( y2<= 89 && y2 >= 78 && x2 == 38.125) // second square
-        {
-          TV.print(35.125,0,"GREAT!");
-          testScore++;
-        } 
-        else {
-          TV.print(35.125,0,"MISS");
-          testScore--;
-        }
-        
-  } // end first curly brace
-} // ending curly brace function
-
-
-void readButtonThree (double x3, double y3,int buttonThree) 
-{ // open curly brace function
- if(buttonThree == HIGH) 
-  { // first curly brace
-    
-      if(y3 <= 89 && y3 >= 78 && x3 == 67.875) // first square
-      { // curly if y
-          TV.print(64.875,0,"GREAT!");
-          testScore++;
-        } 
-        else {
-          TV.print(64.875,0,"MISS");
-          testScore--;
-        }
-        
-  } // end first curly brace
-} // ending curly brace function
-
+}
 
 void createSquare(int x, int y, int sizeX, int sizeY, int color) {
-    TV.draw_rect(x,y,sizeX,sizeY,color);
-    TV.draw_rect(x,y-2,sizeX,sizeY,BLACK);
+    TV.draw_rect(x,y,sizeX,sizeY,color,color);
+    TV.draw_rect(x,y-10,sizeX,sizeY,BLACK,BLACK);
 }
 
 void createGUI() {
@@ -166,10 +176,17 @@ void createGUI() {
   TV.draw_line(86.25,8,86.25,94.5,WHITE); //right-line
   TV.draw_line(0,8,86.25,8,WHITE); // top-line 
   TV.draw_line(0,94.5,86.25,94.5,WHITE); // bottom-line
-  TV.draw_line(0,80,86.25,80,WHITE); // seperator  
+  
+  TV.draw_line(0,80,6.25,80,WHITE); // seperator  
+  TV.draw_line(21.75,80,36.75,80,WHITE); // seperator  
+  TV.draw_line(51.50,80,65.25,80,WHITE); // seperator  
+  TV.draw_line(80.00,80,86.25,80,WHITE); // seperator  
+
+
   TV.print(90,10,"SCORE:"); // String Score ---
 }
 
+/**TEST CODE
 void drawMenu() {
   int z = 0;
   int w = 0;
@@ -187,48 +204,14 @@ void drawMenu() {
   buttonSwitchTwo = digitalRead(buttonTwo);
   buttonSwitchThree = digitalRead(buttonThree);
   
-  while (buttonSwitchOne == LOW /*||  buttonSwitchTwo == LOW || buttonSwitchThree == LOW*/)
+  while (buttonSwitchOne == LOW )
   {
-      if(buttonSwitchOne == HIGH /*||  buttonSwitchTwo == HIGH || buttonSwitchThree == HIGH*/) 
+      if(buttonSwitchOne == HIGH )
       { // first curly brace
         TV.clear_screen();
-        createGUI();
-            
+       // loop();            
       } // end first curly brace
   }
-
-
-
-  /*
-  delay(1000);
-  while(!button1Status) {
-    processInputs();
-    TV.delay_frame(3);
-    if(x + volX < 1 || x + volX > TV.horz_res() - 1) volX = -volX;
-    if(y + volY < 1 || y + volY > TV.vert_res() - 1) volY = -volY;
-    if(TV.get_pixel(x + volX, y + volY)) {
-      TV.set_pixel(x + volX, y + volY, 0);
-    
-      if(TV.get_pixel(x + volX, y - volY) == 0) {
-        volY = -volY;
-      }
-      else if(TV.get_pixel(x - volX, y + volY) == 0) {
-        volX = -volX;
-      }
-      else {
-        volX = -volX;
-        volY = -volY;
-      }
-    }
-    TV.set_pixel(x, y, 0);
-    x += volX;
-    y += volY;
-    TV.set_pixel(x, y, 1);
-  }
- */
-
-  TV.select_font(font4x6);
- // state = IN_GAMEA;
-}
+}**/
 
 
